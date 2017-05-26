a834b7d433bdbec077d03545b056175ad51194ce
Ian Fosbery <ian.fosbery@reaktor.com>
2017-05-26 12:59:33 -0400

    Alias Server in tests

--- ch_16/stack/test/server/server_test.exs
+++ ch_16/stack/test/server/server_test.exs
@@ -1,20 +1,21 @@
 defmodule Stack.ServerTest do
   use ExUnit.Case
+  alias Stack.Server
 
   test "it should pop the last value off of the init'd stack" do
-    Stack.Server.start_link(["ian", 70, "other"])
+    Server.start_link(["ian", 70, "other"])
     assert "ian" == Stack.Server.pop
   end
 
   test "it should push a value to the top of the stack" do
-    Stack.Server.start_link(["ian", 70, "other"])
-    Stack.Server.push("tother")
+    Server.start_link(["ian", 70, "other"])
+    Server.push("tother")
     assert ["tother", "ian", 70, "other"] == Stack.Server.stack()
   end
 
   test "it should terminate when told to do so" do
-    Stack.Server.start_link(["ian", 70, "other"])
-    assert :ok == Stack.Server.shuterdown()
+    Server.start_link(["ian", 70, "other"])
+    assert :ok == Server.shuterdown()
     #catch_exit(Stack.Server.stack())
   end
 end
