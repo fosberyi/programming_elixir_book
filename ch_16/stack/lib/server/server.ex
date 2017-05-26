14c02ef295095224b2416eddf8003c06d13e4074
Ian Fosbery <ian.fosbery@reaktor.com>
2017-05-26 12:12:25 -0400

    Redo way pop works, add terminate clause

--- ch_16/stack/lib/server/server.ex
+++ ch_16/stack/lib/server/server.ex
@@ -18,16 +18,30 @@ defmodule Stack.Server do
     GenServer.cast(__MODULE__, {:push, val})
   end
 
+  def shuterdown do
+    GenServer.cast(__MODULE__, {:shutdown, :bye})
+  end
+
   def handle_call(:stack, _from, stack) do
     {:reply, stack, stack}
   end
 
   def handle_call(:pop, _from, stack) do
-    {:reply, List.last(stack), List.delete_at(stack, -1)}
+    [top | tail] = stack
+    {:reply, top, tail}
   end
 
   def handle_cast({:push, val}, stack) do
     { :noreply, [val | stack]}
+  end
+
+  def handle_cast({:shutdown, :bye}, stack) do
+    {:stop, :shutdown, stack}
+  end
+
+  def terminate(reason, state) do
+    IO.puts "Shutting down because: #{reason}, my final state #{inspect state}"
+    :ok
   end
 end
 
--- ch_16/stack/test/server/server_test.exs
+++ ch_16/stack/test/server/server_test.exs
@@ -3,12 +3,18 @@ defmodule Stack.ServerTest do
 
   test "it should pop the last value off of the init'd stack" do
     Stack.Server.start_link(["ian", 70, "other"])
-    assert "other" == Stack.Server.pop
+    assert "ian" == Stack.Server.pop
   end
 
   test "it should push a value to the top of the stack" do
     Stack.Server.start_link(["ian", 70, "other"])
     Stack.Server.push("tother")
     assert ["tother", "ian", 70, "other"] == Stack.Server.stack()
+  end
+
+  test "it should terminate when told to do so" do
+    Stack.Server.start_link(["ian", 70, "other"])
+    assert :ok == Stack.Server.shuterdown()
+    #catch_exit(Stack.Server.stack())
   end
 end
